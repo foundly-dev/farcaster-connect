8e29adc9836af2342982641eeaed05076741a001
mfbevan <matthewbevan00@gmail.com>
2025-11-04 18:29:17 +1000

    feat: ✨ manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -13,6 +13,13 @@ import {
 
 export async function GET() {
   return Response.json({
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjM4NTY1MSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEM5NEY3MkVERTVmYWZFQmExODIxODAyZUU3YmNmZEYyZjc0MzYzRkQifQ",
+      payload: "eyJkb21haW4iOiJmY2Nvbm5lY3QueHl6In0",
+      signature:
+        "ASsLCB2//W5Dj82GbeRKgskeWVsmmn0j9qy1YcbXsO5U2+WALGR/TZPrfmEY9tyKaiBBqQhB8Xw4jUb87veIMBw=",
+    },
     baseBuilder: {
       allowedAddresses: ["0xeAbEc05d7c47EFf16c279d9091F4008324c693cC"],
     },
